3a1bcb17e79f34865c2c02c7ed6765d60fcfb2f5
Karthikeyan7M <146553507+Karthikeyan7M@users.noreply.github.com>
2024-07-10 21:41:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let typed = new Typed(".changing-text", {
+  strings: [
+    "Front-end Developer",
+    "software Developer",
+    "MERN-stack Developer",
+  ],
+  typeSpeed: 50,
+  backSpeed: 50,
+  loop: true,
+});
